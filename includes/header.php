ce6e27bcfc1e93d88bec0362249cb8404151187b
Vyshnav M R <145757020+vyshnavmr@users.noreply.github.com>
2026-07-27 17:53:38 +0530

    Send contact enquiries via WhatsApp

--- includes/header.php
+++ includes/header.php
@@ -19,7 +19,7 @@ $navItems = [
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600;700&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css?v=<?= filemtime(__DIR__ . '/../assets/css/style.css'); ?>">
 </head>
 <body>
 <header class="site-header">
